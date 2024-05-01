bb25fb36dce1341770c8d12d7bcb9892819256d0
AcceptableDeveloper <justinassinkunas@gmail.com>
2024-05-01 12:22:36 +0100

    Remove unused property

--- city-autocomplete/src/app/core/inputs/autocomplete-cities/autocomplete-cities.service.ts
+++ city-autocomplete/src/app/core/inputs/autocomplete-cities/autocomplete-cities.service.ts
@@ -1,14 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { City } from './city';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutocompleteCitiesService {
   http = inject(HttpClient);
-  storedCities: City[] = [];
 
   public getCities(query: string): Observable<any> {
     return this.http.get<any>(`http://localhost:8000?city=${query}`);
